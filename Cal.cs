fff94dc2f6fd68457fc62c397d96fe3223c6daab
xuanhuy <dinhxuanhuy.pro@gmail.com>
2024-07-13 00:29:26 +0700

    update subtract

--- Cal.cs
+++ Cal.cs
@@ -4,11 +4,11 @@ class Calculator
 {
     public double Add(double a, double b)
     {
-        return a + b;
     }
 
     public double Subtract(double a, double b)
     {
+        return a-b;
     }
 
     public double Multiply(double a, double b)
