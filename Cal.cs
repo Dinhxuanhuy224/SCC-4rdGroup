e1e1590f742fe6688963dbcde27dbe2aa64aa502
xuanhuy <dinhxuanhuy.pro@gmail.com>
2024-07-13 00:01:59 +0700

    origin cal

---
+++ Cal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Calculator
+{
+    public double Add(double a, double b)
+    {
+    }
+
+    public double Subtract(double a, double b)
+    {
+    }
+
+    public double Multiply(double a, double b)
+    {
+    }
+
+    public double Divide(double a, double b)
+    {
+    }
+}
+}
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calculator = new Calculator();
+
+        Console.WriteLine("Enter the first number:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double sum = calculator.Add(num1, num2);
+        double difference = calculator.Subtract(num1, num2);
+        double product = calculator.Multiply(num1, num2);
+        double quotient = calculator.Divide(num1, num2);
+
+        Console.WriteLine($"Sum of {num1} and {num2} is: {sum}");
+        Console.WriteLine($"Difference of {num1} and {num2} is: {difference}");
+        Console.WriteLine($"Product of {num1} and {num2} is: {product}");
+        Console.WriteLine($"Quotient of {num1} and {num2} is: {quotient}");
+
+        Console.ReadLine();
+    }
+}
